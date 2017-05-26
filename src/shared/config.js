00c94a7e65b518edcf785f1a98caa94ff95fd2bc
Matthew Root <matthew.root@optimizely.com>
2017-05-25 22:15:27 -0700

    Use JSS for CSS-in-JS styles

--- src/shared/config.js
+++ src/shared/config.js
@@ -2,7 +2,7 @@
 
 export const WEB_PORT = process.env.PORT || 8000
 export const STATIC_PATH = '/static'
-export const APP_NAME = 'Hot Fuss'
+export const APP_NAME = 'Hello React'
 
 export const WDS_PORT = 7000
 
@@ -14,3 +14,6 @@ export const IO_DISCONNECT = 'disconnect'
 export const IO_CLIENT_HELLO = 'IO_CLIENT_HELLO'
 export const IO_CLIENT_JOIN_ROOM = 'IO_CLIENT_JOIN_ROOM'
 export const IO_SERVER_HELLO = 'IO_SERVER_HELLO'
+
+export const JSS_SSR_CLASS = 'jss-ssr'
+export const JSS_SSR_SELECTOR = `.${JSS_SSR_CLASS}`
